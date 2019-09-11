d49968defd3ed80712e523710bef9b6dc467cdee
Alouiscious Media <info@reneesoul.com>
2019-09-11 03:53:01 -0700

    Update venues_list.rb

--- tourlikebettye/lib/venues_list.rb
+++ tourlikebettye/lib/venues_list.rb
@@ -1,7 +1,19 @@
 
 
+# require 'pry'
+# require "bundler"
+# require "bundler/setup"
+# require "../lib/venues_list.rb"
+# require 'nokogiri'
+# require 'open-uri'
 
 class BettyeVenues
+    bettyes_venues = []
+    Nokogiri::HTML(open("http://www.bettyelavette.com/tour"))
+    Nokogiri::HTML(open("https://www.jambase.com/band/bettye-lavette"))
 
+     bettyes_venues = bettye_noko.css('.artists--bettye-lavette')
+     bettyes_venues << jambase_noko.css('.jbshow jbevent')
+     p bettyes_venues
 
 end
